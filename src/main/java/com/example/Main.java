53dc05e28769138165ff811dd6300ad94cf35804
alebersardinelli <alessandro.berardinelli@sytac.io>
2017-09-22 12:16:05 +0200

    test sh script

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -95,7 +95,7 @@ public class Main {
         System.out.println("STARTED");
 //        String bashScript = getClass().getClassLoader().getResource("bashscript.sh").getPath();
 //      System.out.println("####### running script: " + bashScript);
-        execute("./bashscript.sh");
+        execute("~/bashscript.sh");
         model.put("success", true);
         return "bash";
     }
